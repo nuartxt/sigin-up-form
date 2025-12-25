7597080e7571342aa8b527d52dcd91977bffb820
Anuar <151063105+nuartxt@users.noreply.github.com>
2025-12-25 17:25:20 +0500

    Change width for main_content div

--- js.js
+++ js.js
@@ -1,1 +1,25 @@
 console.log("Hi World!");
+
+
+// let user_data = [{
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     phone: "",
+//     password: ""
+// }];
+
+let user_data = [];
+
+let createAccaunt = document.querySelector("#btn");
+let input = document.querySelectorAll("input");
+
+
+createAccaunt.addEventListener("click", () => {
+    let user = {};
+    input.forEach(el => {
+        user[el.name] = el.value;
+    })
+    user_data.push(user)
+    console.log(user_data);
+})
