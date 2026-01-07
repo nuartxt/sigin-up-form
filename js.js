a0c8dc0b9bb275e62c05096d894cb6e4d2c41ad3
Anuar <151063105+nuartxt@users.noreply.github.com>
2026-01-07 15:26:47 +0500

    add password verification

--- js.js
+++ js.js
@@ -7,27 +7,42 @@ let handler = document.querySelector(".handler");
 let handler_2 = document.querySelector(".handler_2");
 let user_name = document.querySelector("#user_name");
 
+let password = document.querySelector("#password");
+let confirm_password = document.querySelector("#confirm_password");
+
+
+
+
 
 btn.addEventListener("click", () => {
-    //
-    let user = {};
-    input.forEach(el => {
-        user[el.name] = el.value;
-    });
-    user_data.push(user);
-    //
 
 
-    user_name.textContent = user.first_name
+    if (password.value == confirm_password.value) {
 
-    //
-    let user_values = Object.values(user);
-    if (user_values.every(el => el.length > 0)) {
-        handler.style.display = "none";
-        handler_2.style.display = "block";
-        console.log(user);
+        //
+        let user = {};
+        input.forEach(el => {
+            user[el.name] = el.value;
+        });
+        user_data.push(user);
+        //
+
+
+        user_name.textContent = user.first_name
+
+        //
+        let user_values = Object.values(user);
+        if (user_values.every(el => el.length > 0)) {
+            handler.style.display = "none";
+            handler_2.style.display = "block";
+            console.log(user);
+        }
+        //
+
+    } else {
+        alert("The column 'password' and 'confirm password' don't match");
     }
-    //
+
 });
 
 
